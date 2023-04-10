425723a6898454750b5aa581f24ffef5a61c96d0
AM-Ankitgit <mahalleankit007@gmail.com>
2023-04-10 15:01:33 +0530

    Updated

--- src/components/Data_Ingestion.py
+++ src/components/Data_Ingestion.py
@@ -6,6 +6,10 @@ import pandas as pd
 
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
+
+from src.components.Data_Transformation import DataTransformation
+from src.components.Data_Transformation import DataTransformationConfig
+
 # this Decorator method is useful to return perticular specified data type
 # create class variable
 
@@ -50,6 +54,7 @@ class DataIngestion():
             raise CustomException(e, sys)
 if __name__=='__main__':
     obj = DataIngestion()
-    obj.initiate_data_ingestion()
-
+    train_data,test_data=obj.initiate_data_ingestion()
+    data_transformation= DataTransformation()
+    data_transformation.initiate_data_transformation(train_data, test_data)
 
--- src/components/Data_Transformation.py
+++ src/components/Data_Transformation.py
@@ -1,6 +1,5 @@
 
 import sys
-import traceback
 import os
 from dataclasses import dataclass
 
@@ -14,6 +13,7 @@ from sklearn.preprocessing import OneHotEncoder,StandardScaler
 
 from src.exception import CustomException
 from src.logger import logging
+from src.utils import save_object
 
 @dataclass
 class DataTransformationConfig():
@@ -21,12 +21,12 @@ class DataTransformationConfig():
 
 class DataTransformation():
     def __init__(self):
-        self.data_transformation_config=DataTransorformationConfig()
+        self.data_transformation_config=DataTransformationConfig()
     
     def get_data_transformer_object(self):
 
         try:
-            numerical_columns    = ['writing_score','reading']
+            numerical_columns    = ['writing_score','reading_score']
             categorical_columns = [
                 'gender',
                 'race_ethnicity',
@@ -86,5 +86,40 @@ class DataTransformation():
             logging.info('Obtaining preprocessor object')
 
             preprocessor_obj = self.get_data_transformer_object()
-            target_colums_name='math_score'
-            numerical_columns =['writing_sco']
+
+            target_columns_name='math_score'
+            numerical_columns    = ['writing_score','reading_score']
+            
+            input_feature_train_df =train_df.drop(columns=[target_columns_name],axis=1)
+            target_feature_train_df=train_df[target_columns_name]
+
+            input_feature_test_df = test_df.drop(columns=[target_columns_name],axis=1)
+            target_feature_test_df =test_df[target_columns_name]
+
+            logging.info(f'Applying preprocessor object on training dataframe and testing dataframe.')
+
+            input_feature_train_arr = preprocessor_obj.fit_transform(input_feature_train_df)
+            input_feature_test_arr  = preprocessor_obj.fit_transform(input_feature_test_df)
+
+            train_arr = np.c_[
+                input_feature_train_arr,np.array(target_feature_train_df)
+            ]
+            
+            test_arr = np.c_[
+                input_feature_test_arr,np.array(target_feature_test_df)
+            ]
+            
+            logging.info(f'Saved preprocessing object')
+
+            save_object(
+                file_path = self.data_transformation_config.preprocessr_obj_file_path,
+                obj = preprocessor_obj
+            )
+
+            return (
+                train_arr,
+                test_arr,
+                self.data_transformation_config.preprocessr_obj_file_path
+            )
+        except Exception as e:
+            raise CustomException(e, sys)
